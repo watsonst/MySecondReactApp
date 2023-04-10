3e414404294557f4be682e86ab87976e109e966d
Stephanie <me@stephaniemariewatson.com>
2023-04-10 14:51:04 -0400

    Add form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,11 @@
 import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage(){
-    return <div>New Meetup Page</div>;
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm />
+    </section>
 }
 
 export default NewMeetupPage;
