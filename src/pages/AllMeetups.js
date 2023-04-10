a2b38bd631999e933283b14a663c441c56495914
Stephanie <me@stephaniemariewatson.com>
2023-04-10 10:09:46 -0400

    Render list of data by mapping

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+const DUMMYDATA = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description:
+          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+      },
+      {
+        id: 'm2',
+        title: 'This is a second meetup',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Meetupstreet 5, 12345 Meetup City',
+        description:
+          'This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+      },
+]
+
 function AllMeetupsPage(){
-    return <div>All Meetups Page</div>;
+    return <section>
+        <h1>All Meetups</h1>
+        <ul>
+            {DUMMYDATA.map((meetup) => {
+                return <li key={meetup.id}>{meetup.title}</li>
+            })}
+        </ul>
+    </section>;
 }
 
 export default AllMeetupsPage;
