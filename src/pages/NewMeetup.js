c3a6c6becf1c9f1c89f99b0e1662e9229d9e5929
Stephanie <me@stephaniemariewatson.com>
2023-04-10 15:34:29 -0400

    Adding backend and POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,28 @@
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupPage(){
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-a978b-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
+  );
 }
 
 export default NewMeetupPage;
+
+//fetch= get. To change to post add method:
