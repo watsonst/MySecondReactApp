5d80cb55432e77552f2a06de0d42030652eef103
Stephanie <me@stephaniemariewatson.com>
2023-04-10 09:33:22 -0400

    Add navigation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorties";
+import MainNavigation from "./components/MainNavigation";
 
 function App() {
   //localhost:3000
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
