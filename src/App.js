e432cf41c5e93d89ef1f89769d0f2a007ccfaafd
Stephanie <me@stephaniemariewatson.com>
2023-04-10 09:56:09 -0400

    Add navigation style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorties";
-import MainNavigation from "./components/MainNavigation";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   //localhost:3000
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
+import classes from './MainNavigation.module.css'
+
 function MainNavigation() {
-    return <header>
-        <div>React Meetups</div>
+    return <header className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
         <nav>
             <ul>
                 <li>
