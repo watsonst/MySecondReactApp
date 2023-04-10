085734406e8924c52042dc94a8b74ebfccbecc90
Stephanie <me@stephaniemariewatson.com>
2023-04-10 15:13:39 -0400

    Form and user input

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,31 +1,67 @@
 import React from "react";
-import Card from '../ui/Card'
-import classes from './NewMeetupForm.module.css'
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
 
-function NewMeetupForm(){
-    return <Card>
-        <form className={classes.form}>
-            <div className={classes.control}>
-                <lable htmlFor="title">Meetup Title</lable>
-                <input type="text" required id="title" />
-            </div>
-            <div className={classes.control}>
-                <lable htmlFor="image">Meetup Image</lable>
-                <input type='url' required id="image" />
-            </div>
-            <div className={classes.control}>
-                <lable htmlFor="address">Meetup Address</lable>
-                <input type="text" required id="address" />
-            </div>
-            <div className={classes.control}>
-                <lable htmlFor="description">Description</lable>
-                <textarea id='description' required rows='5'></textarea>
-            </div>
-            <div className={classes.actions}>
-                <button>Add Meetup</button>
-            </div>
-        </form>
+function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value; 
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value; 
+
+    const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription
+    }
+
+    console.log(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
     </Card>
+  );
 }
 
-export default NewMeetupForm
+export default NewMeetupForm;
+
+//Don't want browser default behavior of sending new html request automatically.
+//Prevent and run own JS logic without reloading the page.
+//Can get DOM access through refs
+//refs for reading input values
