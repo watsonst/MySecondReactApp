8b3dddf18138be82e5f74fbac8cbffbabc5f8fd5
Stephanie <me@stephaniemariewatson.com>
2023-04-12 10:22:52 -0400

    Render Favorites page and add favorites badge

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css'
+import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
-    return <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">All Meetups</Link>
-                </li>
-                <li>
-                    <Link to="/new-meetup">Add New Meetup</Link>
-                </li>
-                <li>
-                    <Link to="/favorites">My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
 
-export default MainNavigation
+export default MainNavigation;
